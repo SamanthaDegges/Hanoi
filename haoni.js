43a668a5cb84f1c4f0203a664cd989c33bb3efee
Samantha Degges <samanthadegges@gmail.com>
2015-09-01 02:29:47 -0700

    mvp and win state and index.html REINSTATED.

--- haoni.js
+++ haoni.js
@@ -10,14 +10,18 @@ function evalWidthsBeforePrepending(tower) {
   console.log('tower to append to is: '+tower.data("id"));
   currentWidth = $selectedDisc.data("id");
   console.log('$selectedDisc with is: '+currentWidth);
-  var $nextWidth = tower.children().first().data("id"); //queries width of uppermost disc
+  var $nextWidth = tower.children().first().data("id");
   console.log('width of disc beneath is:'+$nextWidth);
   if ($nextWidth > currentWidth) {
     console.log('ta-dah!');
     tower.prepend($selectedDisc);
     $selectedDisc.removeClass("highlight");
+    if (tower.children().length === 3) {
+      winCheck(tower);
+    }
     return $selectedDisc= '';
-    winCheck(tower);
+
+
   } else {
     $selectedDisc.removeClass("highlight");
     return $selectedDisc= '';
@@ -28,7 +32,6 @@ function determine(e){
   if ($selectedDisc) {
     console.log($(this).children());
       if ($(this).children().length>0) {
-        //evaluate widths before adding;
         var towerToPrependTo = $(this);
         evalWidthsBeforePrepending(towerToPrependTo);
       } else {
@@ -42,8 +45,7 @@ function determine(e){
 }
 
 function winCheck(prependedToTower) {
-  console.log(prependedToTower);
-    if (prependedToTower.children().length===3) {
-      console.log("equal to 3.");
-    }
+  $("h5").text("You Win!").addClass("animated bounceInDown");
+  $("#gameBoard").addClass("animated fadeOutDown");//insert animation
+  return $selectedDisc= '';
 }
